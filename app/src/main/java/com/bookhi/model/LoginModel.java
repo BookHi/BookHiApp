45d69bad9b44ac0617cf56fdb250eb700b835d78
Limingyu <limingyu6922430@sina.com>
2017-04-06 01:03:24 +0800

    Add something to empty dir and commit them.

---
+++ app/src/main/java/com/bookhi/model/LoginModel.java
@@ -0,0 +1,8 @@
+package com.bookhi.model;
+
+/**
+ * Created by Limingyu on 2017/4/6.
+ */
+
+public class LoginModel {
+}
---
+++ app/src/main/java/com/bookhi/presenter/LoginPresenter.java
@@ -0,0 +1,8 @@
+package com.bookhi.presenter;
+
+/**
+ * Created by Limingyu on 2017/4/6.
+ */
+
+public class LoginPresenter {
+}
